08c8c705dd7a65135e80e918e482a649d0f09455
Edwin Watkeys <edw@poseur.com>
2019-10-09 10:12:11 -0400

    Update DecimalField.swift

--- DecimalField.swift
+++ DecimalField.swift
@@ -74,78 +74,78 @@ struct DecimalField : View {
     }
 }
 
-struct TipCalculator: View {
-    @State var dollarValue: Decimal?
-    @State var tipRate: Decimal?
-    
-    var tipValue: Decimal? {
-        guard let dollarValue = self.dollarValue, let tipRate = self.tipRate else { return nil }
-        return dollarValue * tipRate
-    }
-    
-    var totalValue: Decimal? {
-        guard let dollarValue = self.dollarValue, let tipValue = self.tipValue else { return nil }
-        return dollarValue + tipValue
-    }
-    
-    static var currencyFormatter: NumberFormatter {
-        let nf = NumberFormatter()
-        nf.numberStyle = .currency
-        nf.isLenient = true
-        return nf
-    }
-    
-    static var percentFormatter: NumberFormatter {
-        let nf = NumberFormatter()
-        nf.numberStyle = .percent
-        nf.isLenient = true
-        return nf
-    }
-    
-    var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                VStack {
-                    HStack {
-                        Text("Check Amount")
-                        Divider()
-                        DecimalField(label: "Amount", value: $dollarValue, formatter: Self.currencyFormatter)
-                    }
-                    
-                    HStack {
-                        Text("Tip Rate")
-                        Divider()
-                        DecimalField(label: "Rate", value: $tipRate, formatter: Self.percentFormatter)
-                    }
-                }
-                .padding()
-                
-                VStack {
-                    HStack {
-                        Text("Tip Amount")
-                        Divider()
-                        Text(Self.currencyFormatter.string(for: tipValue) ?? "-")
-                        Spacer()
-                    }
-                    HStack {
-                        Text("Total")
-                        Divider()
-                        Text(Self.currencyFormatter.string(for: totalValue) ?? "-")
-                        Spacer()
-                    }
-                }
-                .padding()
-                Spacer()
-            }
-            .fixedSize(horizontal: false, vertical: true)
-            .padding()
-        }
-    }
-}
-
 struct DecimalField_Previews: PreviewProvider {
     
     static var previews: some View {
         TipCalculator()
+    }
+    
+    struct TipCalculator: View {
+        @State var dollarValue: Decimal?
+        @State var tipRate: Decimal?
+        
+        var tipValue: Decimal? {
+            guard let dollarValue = self.dollarValue, let tipRate = self.tipRate else { return nil }
+            return dollarValue * tipRate
+        }
+        
+        var totalValue: Decimal? {
+            guard let dollarValue = self.dollarValue, let tipValue = self.tipValue else { return nil }
+            return dollarValue + tipValue
+        }
+        
+        static var currencyFormatter: NumberFormatter {
+            let nf = NumberFormatter()
+            nf.numberStyle = .currency
+            nf.isLenient = true
+            return nf
+        }
+        
+        static var percentFormatter: NumberFormatter {
+            let nf = NumberFormatter()
+            nf.numberStyle = .percent
+            nf.isLenient = true
+            return nf
+        }
+        
+        var body: some View {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    VStack {
+                        HStack {
+                            Text("Check Amount")
+                            Divider()
+                            DecimalField(label: "Amount", value: $dollarValue, formatter: Self.currencyFormatter)
+                        }
+                        
+                        HStack {
+                            Text("Tip Rate")
+                            Divider()
+                            DecimalField(label: "Rate", value: $tipRate, formatter: Self.percentFormatter)
+                        }
+                    }
+                    .padding()
+                    
+                    VStack {
+                        HStack {
+                            Text("Tip Amount")
+                            Divider()
+                            Text(Self.currencyFormatter.string(for: tipValue) ?? "-")
+                            Spacer()
+                        }
+                        HStack {
+                            Text("Total")
+                            Divider()
+                            Text(Self.currencyFormatter.string(for: totalValue) ?? "-")
+                            Spacer()
+                        }
+                    }
+                    .padding()
+                    Spacer()
+                }
+                .fixedSize(horizontal: false, vertical: true)
+                .padding()
+            }
+        }
     }
 }
