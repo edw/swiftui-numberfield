3b2b94bbcbcae1672e80be9a322db28d190f1610
Seb Jachec <sjachec@me.com>
2020-05-20 22:59:35 +0100

    Convert to Swift Package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DecimalField",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "DecimalField", targets: ["DecimalField"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "DecimalField", dependencies: [])
+    ]
+)
